4b1f9b494d61248d6f730d200084e80789120315
Yousefgz <149139058+Yousefgz@users.noreply.github.com>
2023-11-21 15:27:48 +0330

    Add files via upload

---
+++ Session 11/index.php
@@ -0,0 +1,40 @@
+<?php
+$firstname = "yousef";
+$lastname = "Ghavidel";
+$fullname = $firstname.$lastname;
+echo $firstname;
+$age = 20; 
+$score = 18.94; 
+$check = true; 
+echo "<br>";
+echo $age;
+echo "<br>";
+echo $score;
+echo "<br>";
+echo $check;
+echo "<br>";
+echo $fullname;
+echo "<br>";
+
+$x = 10;
+$y = 5;
+
+echo $x+$y;
+echo "<br>";
+
+echo $x-$y;
+echo "<br>";
+
+echo $x*$y;
+echo "<br>";
+
+echo $x/$y;
+echo "<br>";
+
+echo $x%$y;
+echo "<br>";
+
+
+
+?>
+
